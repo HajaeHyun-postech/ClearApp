f3074fe9cacf767ce1cff88f7e5510d74b5cc0f9
leesangoh <dltkdh9845@gmail.com>
2020-08-16 01:32:45 +0900

    <Chore> : <Finish Racket UI - first final version>
    -Finish Racket UI outline for first final, to achieve minimalism the
    most.

--- lib/racket_menu/racket_cardlist.dart
+++ lib/racket_menu/racket_cardlist.dart
@@ -23,47 +23,36 @@ class RacketCardList extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Image.asset(racketCard.image, width: ScreenUtil().setWidth(270), height: ScreenUtil().setHeight(480)),
-          SizedBox(width: ScreenUtil().setWidth(90)),
+          Image.asset(racketCard.image, width: ScreenUtil().setWidth(250), height: ScreenUtil().setHeight(460)),
+          SizedBox(width: ScreenUtil().setWidth(70)),
           Expanded(
-            child:
-            
-          Column(
+            child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-              Text(racketCard.location, 
-              textAlign: TextAlign.end,
-              style: TextStyle(
-                letterSpacing: 0,
-                fontFamily: 'RobotoCondensed',
-                color: Color(0xFF424242),
-                fontWeight: FontWeight.w500,
-                decoration: racketCard.isavailable ? TextDecoration.none : TextDecoration.lineThrough,
-                fontSize: 13,
-              )),
-              Row(
-                children: <Widget>[
-              Text(
-             'No.'+racketCard.id, 
-              textAlign: TextAlign.end,
-              style: TextStyle(
-                letterSpacing: 0,
-                fontFamily: 'RobotoCondensed',
-                color: Color(0xFF424242),
-                fontWeight: FontWeight.w600,
-                fontSize: 11,
-              )),
-              SizedBox(width: ScreenUtil().setWidth(40),),
+                      Text(
+                        racketCard.location, 
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          letterSpacing: 0,
+                          fontFamily: 'RobotoCondensed',
+                          color: Color(0xFF424242),
+                          fontWeight: FontWeight.w600,
+                          )
+                      ),
+                  Row(
+                      children: <Widget>[
+                        !racketCard.isavailable ?  
+                          FaIcon(FontAwesomeIcons.ban, size : ScreenUtil().setWidth(60), color: Color(0xFFFF84B1)) ://Color(0xFFF3F781),) :
+                          FaIcon(FontAwesomeIcons.checkCircle, size : ScreenUtil().setWidth(60), color: ClearAppTheme.green,),
+                          SizedBox(width: ScreenUtil().setWidth(12),),
+                          SizedBox(width: ScreenUtil().setWidth(40),),
+                      ]
+                  )
                 ]
-              )
-             // SizedBox(width: ScreenUtil().setWidth(10)),
-          
-              ]
               ),
               
               SizedBox(
@@ -71,28 +60,24 @@ class RacketCardList extends StatelessWidget {
               ),
               Row(
                 children: <Widget>[
-                  FaIcon(FontAwesomeIcons.infoCircle, size : ScreenUtil().setWidth(52), color: Color(0xFFF3F781),),
-                  SizedBox(width: ScreenUtil().setWidth(12),),
-              Text(
-                racketCard.isavailable ? 'Availabe' : 'Occupied',
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                fontFamily: 'Alata',
-                fontWeight: FontWeight.w300,
-                color: racketCard.isavailable ? ClearAppTheme.green : Color(0xFFFF84B1),
-                fontSize: 11.5,
-                )
-              ),
-              
+                 FaIcon(FontAwesomeIcons.jira, size : ScreenUtil().setWidth(45), color: Color(0xFFCCCED1)),
+                 SizedBox(width: ScreenUtil().setWidth(20)),
+                        Text(
+                    'No.'+racketCard.id, 
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                      letterSpacing: 0,
+                      fontFamily: 'RobotoCondensed',
+                      color: Color(0xFF424242),
+                      fontWeight: FontWeight.w600,
+                      fontSize: 11,
+                      )
+                    ),
                 ],
               ),
-              
             ]
           ), 
-           
-          
           )
-          
         ],
       ),
     );
@@ -100,18 +85,9 @@ class RacketCardList extends StatelessWidget {
     final RacketCardf = Container(
       child: RacketCardContent,
       height: ScreenUtil().setHeight(340),
-       //margin: EdgeInsets.only(left: ScreenUtil().setWidth(30)),
       decoration: BoxDecoration(
-        color: ClearAppTheme.nearlyWhite,
+        color: ClearAppTheme.white,
         shape: BoxShape.rectangle,
-        //borderRadius: BorderRadius.circular(8.0),
-        boxShadow: <BoxShadow>[
-          /*BoxShadow(
-            color: Colors.black12,
-            blurRadius: 18.0,
-            offset: Offset(-10, 10.0),
-          ),*/
-        ],
       ),
     );
     return GestureDetector(
@@ -129,17 +105,16 @@ class RacketCardList extends StatelessWidget {
           child: 
           Column(
            children: <Widget>[
-             
             RacketCardf,
-            Divider(
-              indent: ScreenUtil().setWidth(70),
-              endIndent: ScreenUtil().setWidth(70),
-              color: Color(0xFFF1F4F7),
-              thickness: ScreenUtil().setHeight(6),
-          ),
+                Divider(
+                  indent: ScreenUtil().setWidth(70),
+                  endIndent: ScreenUtil().setWidth(70),
+                  color: Color(0xFFF1F4F7),
+                  thickness: ScreenUtil().setHeight(6),
+              ),
             ]
           )
-           
-        ));
+        )
+    );
   }
 }
