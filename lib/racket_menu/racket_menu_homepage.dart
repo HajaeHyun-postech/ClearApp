dc9ad243d8d1f9f96e90ae442efd6ca13ddcc82c
leesangoh <dltkdh9845@gmail.com>
2020-08-18 00:06:48 +0900

    <Feat> : <Add Filtering Function>
    - Add custom filter to make users easy to find history or status
      according to filtering standard
    - To make admin easy to confirm racket status, I added new tab named
      'All Racket Rent History' if user is admin

--- lib/racket_menu/racket_menu_homepage.dart
+++ lib/racket_menu/racket_menu_homepage.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import '../util/appbar.dart';
-import '../util/toast_generator.dart';
 import 'racket_cardlist.dart';
 import 'racket_card.dart';
 import '../util/app_theme.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart'; 
 import 'package:flutter/cupertino.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:provider/provider.dart';
-import '../util/toast_generator.dart';
+import 'custom_filter.dart';
 
 class RacketmenuHomepage extends StatelessWidget{
   @override
@@ -63,7 +60,10 @@ class _RacketScrollView extends State<RacketScrollView>{
           controller: _scrollController,
           slivers: <Widget>[
             SliverList(delegate: SliverChildBuilderDelegate(
-                  (context, index) => CustomFilter(),
+                  (context, index) => Row( children: <Widget>[
+                    SizedBox(width: ScreenUtil().setWidth(70)),
+                    CustomFilter(),
+                    ]),
                   childCount: 1,
                 ),
             ),
@@ -81,50 +81,3 @@ class _RacketScrollView extends State<RacketScrollView>{
       );
   }
 }
-
-
-class CustomFilter extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: ClearAppTheme.white,
-      child: Row(
-      children: <Widget>[
-        SizedBox(width: ScreenUtil().setWidth(70)),
-          InkWell(
-            borderRadius: const BorderRadius.all(
-                      Radius.circular(2.0),
-                    ),
-            onTap: () { 
-        }
-        ,
-            child:
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal : ScreenUtil().setWidth(0), vertical: ScreenUtil().setHeight(5)),
-              child:
-                      Row(
-                        children: <Widget>[
-                          Text('Result',
-                            style: TextStyle(
-                              fontFamily: 'RobotoCondensed',
-                              fontWeight: FontWeight.w600,
-                              fontSize:15,
-                            ),
-                          ),
-                          SizedBox(width: ScreenUtil().setWidth(30)),
-                          FaIcon(FontAwesomeIcons.caretDown, 
-                              size: ScreenUtil().setWidth(45),
-                              color: ClearAppTheme.darkBlue,
-                              ),
-                          ]
-                      )
-                  
-              )
-
-        )
-      ]
-    )
-    );
-  }
-}
-
--- lib/racket_menu/racketdata_manage/events.dart
+++ lib/racket_menu/racketdata_manage/events.dart
@@ -1,7 +1,46 @@
+import '../../login/login_info.dart';
+
 enum EVENT{
   RacketRentEvent,
   RacketReturnEvent,
   FilterMyHstrEvent,
   FilterMyRacketStatusEvent,
   FilterAllRacketStatusEvent,
+  FilterAllRacketHstrEvent, // for admin
+}
+
+class RacketMenu{
+  final EVENT eventType;
+  final String menu;
+  RacketMenu({this.eventType, this.menu});
+}
+
+class RacketMenus{
+ 
+  static List<RacketMenu> returnMenus(){
+    List<RacketMenu> menus = [
+    RacketMenu(
+      eventType: EVENT.FilterMyHstrEvent,
+      menu: 'My History',
+    ),
+    RacketMenu(
+      eventType: EVENT.FilterMyRacketStatusEvent,
+      menu: 'My Status',
+    ),
+    RacketMenu(
+      eventType: EVENT.FilterAllRacketStatusEvent,
+      menu: 'Rent',
+    ),
+  ];
+  if(LoginInfo().isAdmin == true){
+      RacketMenu adminFilter = 
+        new RacketMenu(
+          eventType: EVENT.FilterAllRacketHstrEvent,
+          menu: 'All Racket Rent History',
+        );
+
+      menus.add(adminFilter);
+  }
+    return menus;
+  }
 }
