04e04f1bfd4212cd6a7d819a63cdff4ac2e306a7
leesangoh <dltkdh9845@gmail.com>
2020-08-22 03:29:36 +0900

    <Chore> : <Add new design of rent window>
    - used pastel tone to fit toatl application theme
    - used minimal icons to make users easy to understand

--- lib/ui/racket_menu/rent_window.dart
+++ lib/ui/racket_menu/rent_window.dart
@@ -26,26 +26,16 @@ class _RentWindowState extends State<RentWindow> {
         children: <Widget>[
           Container(
             margin: EdgeInsets.fromLTRB(
-                ScreenUtil().setWidth(200),
+                ScreenUtil().setWidth(130),
                 ScreenUtil().setHeight(70),
-                ScreenUtil().setWidth(210),
+                ScreenUtil().setWidth(0),
                 ScreenUtil().setHeight(0)),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text(racketCard.name,
-                    textAlign: TextAlign.end,
-                    style: TextStyle(
-                      letterSpacing: 0,
-                      fontFamily: 'RobotoCondensed',
-                      color: Color(0xFF837F76),
-                      fontWeight: FontWeight.w500,
-                      fontSize: ScreenUtil().setSp(100),
-                      //fontSize: 14,
-                    )),
                 ClipRRect(
-                  //borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.circular(8.0),
                   child: Image.asset(
                     racketCard.asset,
                     width: ScreenUtil().setWidth(155),
@@ -53,19 +43,46 @@ class _RentWindowState extends State<RentWindow> {
                     fit: BoxFit.fill,
                   ),
                 ),
+                SizedBox(width: ScreenUtil().setWidth(56)),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text('No.' + racketCard.id.toString(),
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          letterSpacing: 0,
+                          fontFamily: 'RobotoCondensed',
+                          color: Color(0xFF837F76),
+                          fontWeight: FontWeight.w500,
+                          fontSize: ScreenUtil().setSp(75),
+                          //fontSize: 14,
+                        )),
+                    SizedBox(width: ScreenUtil().setWidth(56)),
+                    Text(racketCard.name,
+                        textAlign: TextAlign.end,
+                        style: TextStyle(
+                          letterSpacing: 0,
+                          fontFamily: 'RobotoCondensed',
+                          color: Color(0xFF837F76),
+                          fontWeight: FontWeight.w500,
+                          fontSize: ScreenUtil().setSp(90),
+                          //fontSize: 14,
+                        )),
+                  ],
+                ),
               ],
             ),
           ),
-          SizedBox(height: ScreenUtil().setHeight(50)),
+          SizedBox(height: ScreenUtil().setHeight(100)),
           Container(
             color: Colors.transparent,
             margin: EdgeInsets.fromLTRB(
-                ScreenUtil().setWidth(70),
+                ScreenUtil().setWidth(250),
                 ScreenUtil().setHeight(0),
-                ScreenUtil().setWidth(70),
+                ScreenUtil().setWidth(250),
                 ScreenUtil().setHeight(0)),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 Column(
                   children: <Widget>[
@@ -74,25 +91,9 @@ class _RentWindowState extends State<RentWindow> {
                       size: ScreenUtil().setWidth(80),
                       color: Color(0xFFCECEB8),
                     ),
+                    SizedBox(height: ScreenUtil().setHeight(15)),
                     Text(
                       racketCard.weight.toString() + 'U',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: ScreenUtil().setSp(50),
-                        color: Color(0xFF837F76),
-                      ),
-                    )
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    FaIcon(
-                      FontAwesomeIcons.balanceScale,
-                      size: ScreenUtil().setWidth(80),
-                      color: Color(0xFFCECEB8),
-                    ),
-                    Text(
-                      racketCard.balance.toString(),
                       style: TextStyle(
                         fontFamily: 'Roboto',
                         fontSize: ScreenUtil().setSp(50),
@@ -108,6 +109,7 @@ class _RentWindowState extends State<RentWindow> {
                       size: ScreenUtil().setWidth(80),
                       color: Color(0xFFCECEB8),
                     ),
+                    SizedBox(height: ScreenUtil().setHeight(15)),
                     Text(
                       racketCard.type,
                       style: TextStyle(
@@ -118,7 +120,53 @@ class _RentWindowState extends State<RentWindow> {
                     )
                   ],
                 ),
+                Column(
+                  children: <Widget>[
+                    FaIcon(
+                      FontAwesomeIcons.balanceScale,
+                      size: ScreenUtil().setWidth(80),
+                      color: Color(0xFFCECEB8),
+                    ),
+                    SizedBox(height: ScreenUtil().setHeight(15)),
+                    Text(
+                      racketCard.balance.toString(),
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: ScreenUtil().setSp(50),
+                        color: Color(0xFF837F76),
+                      ),
+                    )
+                  ],
+                ),
               ],
+            ),
+          ),
+          SizedBox(
+            height: ScreenUtil().setHeight(100),
+          ),
+          InkWell(
+            onTap: null,
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: Color(0xFFF7F2E7),
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Container(
+                width: ScreenUtil().setWidth(1200),
+                height: ScreenUtil().setHeight(130),
+                child: Center(
+                  child: Text(
+                    "Rent now",
+                    style: TextStyle(
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w600,
+                      fontSize: ScreenUtil().setSp(60),
+                      color: Color(0xFF837F76),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
         ],
