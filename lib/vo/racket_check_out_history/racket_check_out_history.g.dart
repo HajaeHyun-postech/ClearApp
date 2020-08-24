58c0fed41e1a96d6efb2688a1d8d7779a9f740d0
hajae <rntlqvnf@naver.com>
2020-08-24 19:45:23 +0900

    <Chore> : Update store

--- lib/vo/racket_check_out_history/racket_check_out_history.g.dart
+++ lib/vo/racket_check_out_history/racket_check_out_history.g.dart
@@ -10,11 +10,19 @@ RacketCheckOutHistory _$RacketCheckOutHistoryFromJson(
     Map<String, dynamic> json) {
   return RacketCheckOutHistory(
     json['id'] as int,
-    User.fromJson(json['user'] as Map<String, dynamic>),
-    Racket.fromJson(json['racket'] as Map<String, dynamic>),
-    DateTime.parse(json['rentDate'] as String),
-    DateTime.parse(json['dueDate'] as String),
-    DateTime.parse(json['returnDate'] as String),
+    json['user'] == null
+        ? null
+        : User.fromJson(json['user'] as Map<String, dynamic>),
+    json['racket'] == null
+        ? null
+        : Racket.fromJson(json['racket'] as Map<String, dynamic>),
+    json['rentDate'] == null
+        ? null
+        : DateTime.parse(json['rentDate'] as String),
+    json['dueDate'] == null ? null : DateTime.parse(json['dueDate'] as String),
+    json['returnDate'] == null
+        ? null
+        : DateTime.parse(json['returnDate'] as String),
   );
 }
 
@@ -24,7 +32,7 @@ Map<String, dynamic> _$RacketCheckOutHistoryToJson(
       'id': instance.id,
       'user': instance.user,
       'racket': instance.racket,
-      'rentDate': instance.rentDate.toIso8601String(),
-      'dueDate': instance.dueDate.toIso8601String(),
-      'returnDate': instance.returnDate.toIso8601String(),
+      'rentDate': instance.rentDate?.toIso8601String(),
+      'dueDate': instance.dueDate?.toIso8601String(),
+      'returnDate': instance.returnDate?.toIso8601String(),
     };
